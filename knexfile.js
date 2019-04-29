9dc5964d091687cc98f1ddac255bad47573ad344
Dil Sombayeva <dilyasom@gmail.com>
2019-04-29 08:54:42 -0700

    add db sqlite3

--- knexfile.js
+++ knexfile.js
@@ -7,5 +7,17 @@ module.exports = {
    filename: './data/dev.sqlite3',
   },
   useNullAsDefault: true, // needed for sqlite
+  migrations: {
+   directory: './data/migrations',
+  },
+  seeds: {
+   directory: './data/seeds',
+  },
+  // by default SQLite will not enforce foreign keys
+  pool: {
+   afterCreate: (conn, done) => {
+    conn.run('PRAGMA foreign_keys = ON', done); // enforce FK
+   },
+  },
  },
 };
