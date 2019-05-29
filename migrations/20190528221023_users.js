fea2e55f8a85a78d2af3f01d9725119e368b60d9
Kristea Gamel <krgamel@gmail.com>
2019-05-28 22:22:21 -0500

    added seed

---
+++ migrations/20190528221023_users.js
@@ -0,0 +1,19 @@
+// what changes are to be applied to theh database
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('users', function(tbl) {
+        // primary key called id, integer, auto-increment
+        
+        tbl.increments();
+  
+        tbl.string('name', 128).notNullable();
+
+        tbl.integer('role_id').unsigned().references('id').inTable('roles').onDelete('CASCADE').onUpdate('CASCADE');
+
+        tbl.timestamps(true, true);
+    })
+  };
+  
+  //how can i undo the changes
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('users');
+  };
